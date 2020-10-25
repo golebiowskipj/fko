ef6043ac541bcad460adb5fb840e1349d292877e
golebiowskipj@gmail.com <golebiowskipj@gmail.com>
2020-10-25 18:56:19 +0100

    typo in route

--- src/configs/routes.js
+++ src/configs/routes.js
@@ -3,4 +3,4 @@ export const HOME = "/rezerwuj";
 export const LANDING = "/";
 export const MY_RESERVATIONS = "/moje-rezerwacje";
 export const SIGN_UP = "/zarejestruj";
-export const SIGN_IN = "/zaloguje";
+export const SIGN_IN = "/zaloguj";
