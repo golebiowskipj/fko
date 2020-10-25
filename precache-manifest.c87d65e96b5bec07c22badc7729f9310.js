3a1eb499d5df642c9c08564eb16011ee6821f1d0
golebiowskipj@gmail.com <golebiowskipj@gmail.com>
2020-10-25 18:45:11 +0100

    Updates

---
+++ precache-manifest.c87d65e96b5bec07c22badc7729f9310.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e4c2b2bb2dfcde6a449f944028515d",
+    "url": "/fko/index.html"
+  },
+  {
+    "revision": "be36321835e1e5320438",
+    "url": "/fko/static/css/2.e6ac38db.chunk.css"
+  },
+  {
+    "revision": "be36321835e1e5320438",
+    "url": "/fko/static/js/2.c75e1798.chunk.js"
+  },
+  {
+    "revision": "5e4bce772c515fa05499a2671655bd86",
+    "url": "/fko/static/js/2.c75e1798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "730f0e86fc8853c6799e",
+    "url": "/fko/static/js/main.83d2d3f4.chunk.js"
+  },
+  {
+    "revision": "ce2f848bcf8b9da25eed",
+    "url": "/fko/static/js/runtime-main.8fa67ccd.js"
+  },
+  {
+    "revision": "fc566f5e595e5acd380e5e1f08d94df5",
+    "url": "/fko/static/media/logo.fc566f5e.png"
+  }
+]);
