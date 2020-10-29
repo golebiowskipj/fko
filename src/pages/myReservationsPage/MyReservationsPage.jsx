5be594171b8b6bbfbc849987d8d41a70020cc965
golebiowskipj@gmail.com <golebiowskipj@gmail.com>
2020-10-29 12:35:01 +0100

    hide my reservations page bug

--- src/pages/myReservationsPage/MyReservationsPage.jsx
+++ src/pages/myReservationsPage/MyReservationsPage.jsx
@@ -12,45 +12,46 @@ import { labels } from "../../configs/labels";
 import { List, Wrapper } from "./styled";
 
 export const MyReservationsPage = () => {
-  const [usersReservations, setUsersReservations] = useState([]);
-  const { userData } = useContext(AppDataContext);
-  const firebaseContext = useContext(FirebaseContext);
+  // const [usersReservations, setUsersReservations] = useState([]);
+  // const { userData } = useContext(AppDataContext);
+  // const firebaseContext = useContext(FirebaseContext);
 
-  useEffect(() => {
-    const getReservations = async () => {
-      const user = await firebaseContext.getUser(userData.uid);
+  // useEffect(() => {
+  //   const getReservations = async () => {
+  //     const user = await firebaseContext.getUser(userData.uid);
 
-      const { assignedTo } = user;
+  //     const { assignedTo } = user;
 
-      const decodedAssignedTo = assignedTo.map((item) => {
-        const array = item.split("-");
-        const [date, name] = array;
+  //     const decodedAssignedTo = assignedTo.map((item) => {
+  //       const array = item.split("-");
+  //       const [date, name] = array;
 
-        return { name, date };
-      });
+  //       return { name, date };
+  //     });
 
-      const filteredAssignedTo = decodedAssignedTo.filter(
-        (d) => d.date >= getTodaysMidnight()
-      );
+  //     const filteredAssignedTo = decodedAssignedTo.filter(
+  //       (d) => d.date >= getTodaysMidnight()
+  //     );
 
-      setUsersReservations(filteredAssignedTo);
-    };
+  //     setUsersReservations(filteredAssignedTo);
+  //   };
 
-    getReservations();
-    // eslint-disable-next-line
-  }, []);
+  //   getReservations();
+  //   // eslint-disable-next-line
+  // }, []);
 
-  return (
-    <Wrapper>
-      <List>
-        {usersReservations.length === 0
-          ? labels.youAreNotAssignedYet
-          : usersReservations.map((t, idx) => (
-              <li key={idx}>
-                {convertDateToHumanReadable(Number(t.date))} {t.name}
-              </li>
-            ))}
-      </List>
-    </Wrapper>
-  );
+  // return (
+  //   <Wrapper>
+  //     <List>
+  //       {usersReservations.length === 0
+  //         ? labels.youAreNotAssignedYet
+  //         : usersReservations.map((t, idx) => (
+  //             <li key={idx}>
+  //               {convertDateToHumanReadable(Number(t.date))} {t.name}
+  //             </li>
+  //           ))}
+  //     </List>
+  //   </Wrapper>
+  // );
+  return <p>moje rezerwacje</p>;
 };
